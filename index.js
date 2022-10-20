7150fde03a60c6373fb6af286e49e64a0b34a5be
Clovis Santiago <sant_sivolc@hotmail.com>
2022-10-19 21:56:02 -0300

    adc alet

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const multiplicacao = [65, 44, 12,  4]
+alert ("Para ver o resultado siga as etapas: 1º Aperte f12 para abrir janela inspecionar na lateral direita da tela, 2º Nesta janela vá no menu console, 3º Click em ok para ver o resultado :)")
+const multiplicacao = [65, 44, 12, 4]
 
 multiplicacao.forEach(function(item){
 console.log('Resultado da multiplicação é : ' + item *10)
